d294ff408cdde56186955dd798bd6186b84ed722
emhopkins <epoletto@lstreetc.com>
2018-02-11 10:13:56 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
-ReactDOM.render(
-  meInReact,
-  document.getElementById('global')
-);
+// const meInReact = ""
 
-export default meInReact
+// ReactDOM.render(
+//   meInReact,
+//   document.getElementById('global')
+// );
 
-const list =
+
+
+const meInReact =
   React.createElement('div', { className: 'me'},
     React.createElement('h1', {}, 'An Awesome Person'),
     React.createElement('p', {}, 'Who is learning React'),
@@ -19,13 +21,14 @@ const list =
         React.createElement('li', {}, 'JavaScript'),
         React.createElement('li', {}, 'React'),
         React.createElement('li', {}, 'Movies'),
-        React.createElement('li', {}, 'Icecream')
+        React.createElement('li', {}, 'Ice cream')
       ]
     )
   );
  
 ReactDOM.render(
-  list,
+  meInReact,
   document.getElementById('global')
 );
 
+export default meInReact
